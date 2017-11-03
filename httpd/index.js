f8988b97b031b1dd3e8cd528f57c694692d8667f
Wayzro <asd71402asd@gmail.com>
2017-11-03 15:57:06 +0800

    index.js連結index.html css png。

--- httpd/index.js
+++ httpd/index.js
@@ -2,20 +2,83 @@
 
  let http = require('http');
 
- http.createServer((request, response) => {
-   // 取得 node.js 的 fs 模組
- let fs = require('fs')
 
-fs.readFile('../htdocs/index.html', (err, data) => {
-  response.writeHead(200, {
+http.createServer((request, response) => {
+   request.on('end', () => {
+   console.log('Request method: ' + request.method);
+   console.log('Request url: ' + request.url);
+   });
+
+   let fs = require('fs');
+   let postData = '';
+
+request.on('data', (chunk) =>{
+ postData += chunk;
+
+console.log(
+' 接收的 POST data ⽚段 k: [' + chunk + '].'
+ );
+});
+
+ request.on('end', () => {
+ switch (request.url) {
+ case '/':
+ fs.readFile('../htdocs/index.html', (err, data) => {
+ if (err) {
+ console.log(' 檔案讀取錯誤');
+ }
+ else {
+ response.writeHead(200, {
  'Content-Type': 'text/html'
   });
+ response.write(data);
+ response.end();
+ }
+ });
+ break;
 
-  response.write(data);
 
-  response.end();
+ case '/assets/css/styles.css':
+ fs.readFile('C:/wayzro/htdocs/assets/css/styles.css', (err, data) => {
+ if (err) {
+ console.log(' 檔案讀取錯誤');
+ }
+ else {
+ response.writeHead(200, {
+ 'Content-Type': 'text/css'
   });
-  }).listen(8088);
+ response.write(data);
+ response.end();
+ }
+ });
+ break;
+
+
+
+ case '/assets/png/SokobanClone_byVellidragon.png':
+ fs.readFile('C:/wayzro/htdocs/assets/png/SokobanClone_byVellidragon.png', (err, data) => {
+ if (err) {
+ console.log(' 檔案讀取錯誤');
+ }
+ else {
+ response.writeHead(200, {
+ 'Content-Type': 'image/png'
+  });
+ response.write(data);
+ response.end();
+ }
+ });
+ break;
+
+
+
+ default:
+ console.log(' 未定義的存取: ' + request.url);
+ response.end();
+ break;
+ }
+ });
+ }).listen(8088);
 
  // log message to Console
  console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
