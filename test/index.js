cbfd7f3b15daea317b60748e281a014802d59845
Wayzro <asd71402asd@gmail.com>
2017-12-15 13:51:11 +0800

    test 資料夾。

---
+++ test/index.js
@@ -0,0 +1,11 @@
+'use strict';
+ window.addEventListener('load', () => {
+ console.log("index.js loaded");
+
+ let h1 = document.createElement('h1');
+ let msg = document.createTextNode(' 這是 <h1> 的⽂字訊息');
+
+ h1.appendChild(msg);
+
+ document.body.appendChild(h1);
+ });
